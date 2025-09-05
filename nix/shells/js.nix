493c44de59fefb05ccde80d413c577df80d74124
Loris Jautakas <lorisjautakas@gmail.com>
2025-09-05 14:41:05 -0700

    fixed names, added tsconfig to ui

--- nix/shells/js.nix
+++ nix/shells/js.nix
@@ -3,6 +3,7 @@
   name = "js";
   buildInputs = with pkgs; [
     nodejs
+    npkill   # removes node_modules directories
     nodePackages.pnpm
     nodePackages.typescript
     nodePackages.typescript-language-server
