dd2c38036d568a17b0d15ae1983ac7f3a526e5be
Loris Jautakas <lorisjautakas@gmail.com>
2025-09-14 11:39:23 -0700

    betterproto ->  protobuff/python

--- apps/website1/features/grpc-client.ts
+++ apps/website1/features/grpc-client.ts
@@ -1,9 +1,10 @@
 import { createClient } from "@connectrpc/connect";
 import { createConnectTransport } from "@connectrpc/connect-web";
-import { ExampleService } from "@repo/proto/generated/ts-grpc/example/v1/example_pb";
+import { ExampleService } from "@repo/proto/generated/ts/example/v1/example_pb";
 
 const transport = createConnectTransport({
         baseUrl : "http://demo.connectrpc.com",
+        
       });
 
 export const client: ReturnType<typeof createClient<typeof ExampleService>> = createClient(ExampleService, transport);
